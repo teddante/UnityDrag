65c94f9b2f0adce80782b1188b81accbb6842919
Ted Enston <edwardenston@gmail.com>
2022-07-12 14:35:02 +0100

    Added static World class for world field control

--- Unity Drag/Assets/Scripts/Drag.cs
+++ Unity Drag/Assets/Scripts/Drag.cs
@@ -5,52 +5,50 @@ namespace Assets
 {
     public class Drag : MonoBehaviour
     {
-        public Vector3 WorldWind;
-
         private Mesh DragMesh;
         public Rigidbody DragRigidbody;
 
-        public int[] Triangles;
-        public Vector3[] Vertices;
-        public Vector3[] Normals;
+        private int[] _triangles;
+        private Vector3[] _vertices;
+        private Vector3[] Normals;
 
-        public float[] Area;
-        public Vector3[] WorldTriangleCenter;
-        public Vector3[] WorldTriangleNormal;
-        public Vector3[] MeshTriangleVelocities;
-        public Vector3[] LastTrianglePosition;
-        public float[] Angle;
-        public float[] ForceMagnitude;
-        public Vector3[] Force;
+        private float[] Area;
+        private Vector3[] WorldTriangleCenter;
+        private Vector3[] WorldTriangleNormal;
+        private Vector3[] MeshTriangleVelocities;
+        private Vector3[] LastTrianglePosition;
+        private float[] Angle;
+        private float[] ForceMagnitude;
+        private Vector3[] Force;
 
         void Awake()
         {
             DragMesh = GetComponent<MeshFilter>().mesh;
             //DragRigidbody = GetComponent<Rigidbody>();
 
-            Triangles = DragMesh.triangles;
-            Vertices = DragMesh.vertices;
+            _triangles = DragMesh.triangles;
+            _vertices = DragMesh.vertices;
             Normals = DragMesh.normals;
 
-            Area = new float[Triangles.Length / 3];
-            WorldTriangleCenter = new Vector3[Triangles.Length / 3];
-            WorldTriangleNormal = new Vector3[Triangles.Length / 3];
-            MeshTriangleVelocities = new Vector3[Triangles.Length / 3];
-            LastTrianglePosition = new Vector3[Triangles.Length / 3];
-            Angle = new float[Triangles.Length / 3];
-            ForceMagnitude = new float[Triangles.Length / 3];
-            Force = new Vector3[Triangles.Length / 3];
+            Area = new float[_triangles.Length / 3];
+            WorldTriangleCenter = new Vector3[_triangles.Length / 3];
+            WorldTriangleNormal = new Vector3[_triangles.Length / 3];
+            MeshTriangleVelocities = new Vector3[_triangles.Length / 3];
+            LastTrianglePosition = new Vector3[_triangles.Length / 3];
+            Angle = new float[_triangles.Length / 3];
+            ForceMagnitude = new float[_triangles.Length / 3];
+            Force = new Vector3[_triangles.Length / 3];
 
-            for (int triangle = 0; triangle < Triangles.Length; triangle += 3)
+            for (int triangle = 0; triangle < _triangles.Length; triangle += 3)
             {
-                LastTrianglePosition[triangle / 3] = (transform.TransformPoint(Vertices[Triangles[triangle]]) +
-                                                      transform.TransformPoint(Vertices[Triangles[triangle + 1]]) +
-                                                      transform.TransformPoint(Vertices[Triangles[triangle + 2]])) /
+                LastTrianglePosition[triangle / 3] = (transform.TransformPoint(_vertices[_triangles[triangle]]) +
+                                                      transform.TransformPoint(_vertices[_triangles[triangle + 1]]) +
+                                                      transform.TransformPoint(_vertices[_triangles[triangle + 2]])) /
                                                      3;
 
-                Vector3 corner = transform.TransformPoint(Vertices[Triangles[triangle]]);
-                Vector3 a = transform.TransformPoint(Vertices[Triangles[triangle + 1]]) - corner;
-                Vector3 b = transform.TransformPoint(Vertices[Triangles[triangle + 2]]) - corner;
+                Vector3 corner = transform.TransformPoint(_vertices[_triangles[triangle]]);
+                Vector3 a = transform.TransformPoint(_vertices[_triangles[triangle + 1]]) - corner;
+                Vector3 b = transform.TransformPoint(_vertices[_triangles[triangle + 2]]) - corner;
 
                 int i = triangle / 3;
 
@@ -62,33 +60,33 @@ namespace Assets
         void FixedUpdate()
         {
             //Force at specific triangle selected in loop
-            for (int triangle = 0; triangle < Triangles.Length; triangle += 3)
+            for (int triangle = 0; triangle < _triangles.Length; triangle += 3)
             {
                 int i = triangle / 3;
 
-                WorldTriangleCenter[i] = (transform.TransformPoint(Vertices[Triangles[triangle]]) +
-                                          transform.TransformPoint(Vertices[Triangles[triangle + 1]]) +
-                                          transform.TransformPoint(Vertices[Triangles[triangle + 2]])) /
+                WorldTriangleCenter[i] = (transform.TransformPoint(_vertices[_triangles[triangle]]) +
+                                          transform.TransformPoint(_vertices[_triangles[triangle + 1]]) +
+                                          transform.TransformPoint(_vertices[_triangles[triangle + 2]])) /
                                          3;
 
-                WorldTriangleNormal[i] = (transform.TransformDirection(Normals[Triangles[triangle]]) +
-                                          transform.TransformDirection(Normals[Triangles[triangle + 1]]) +
-                                          transform.TransformDirection(Normals[Triangles[triangle + 2]])) /
+                WorldTriangleNormal[i] = (transform.TransformDirection(Normals[_triangles[triangle]]) +
+                                          transform.TransformDirection(Normals[_triangles[triangle + 1]]) +
+                                          transform.TransformDirection(Normals[_triangles[triangle + 2]])) /
                                          3;
 
                 MeshTriangleVelocities[i] = (WorldTriangleCenter[i] - LastTrianglePosition[i]) / Time.fixedDeltaTime;
 
                 LastTrianglePosition[i] = WorldTriangleCenter[i];
 
-                Angle[i] = Vector3.Angle(MeshTriangleVelocities[i] + WorldWind, WorldTriangleNormal[i]);
+                Angle[i] = Vector3.Angle(MeshTriangleVelocities[i] + World.Wind, WorldTriangleNormal[i]);
 
-                ForceMagnitude[i] = (float)(0.5 * 1.2 * -Mathf.Pow(MeshTriangleVelocities[i].magnitude + WorldWind.magnitude, 2) * Mathf.Clamp(Mathf.Cos(Angle[i] * Mathf.Deg2Rad), 0, 1) * Area[i]);
+                ForceMagnitude[i] = (float)(0.5 * 1.2 * -Mathf.Pow(MeshTriangleVelocities[i].magnitude + World.Wind.magnitude, 2) * Mathf.Clamp(Mathf.Cos(Angle[i] * Mathf.Deg2Rad), 0, 1) * Area[i]);
 
                 Force[i] = WorldTriangleNormal[i] * ForceMagnitude[i];
 
                 DragRigidbody.AddForceAtPosition(Force[i], WorldTriangleCenter[i]);
 
-                DebugDrag(i);
+                if (World.Debug) DebugDrag(i);
             }
         }
 
---
+++ Unity Drag/Assets/Scripts/World.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Assets
+{
+    public static class World
+    {
+        public static bool Debug = false;
+
+        public static Vector3 Wind = Vector3.zero;
+    };
+}
