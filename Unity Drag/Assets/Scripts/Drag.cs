5ba92d23e0ec2d0a7d4720ea8f302e4057c3d9a3
Ted Enston <edwardenston@gmail.com>
2022-07-12 14:16:29 +0100

    Move files and organise

--- Unity Drag/Assets/Scripts/Drag.cs
+++ Unity Drag/Assets/Scripts/Drag.cs
@@ -7,7 +7,7 @@ namespace Assets
     {
         public Vector3 WorldWind;
 
-        public Mesh DragMesh;
+        private Mesh DragMesh;
         public Rigidbody DragRigidbody;
 
         public int[] Triangles;
@@ -15,8 +15,8 @@ namespace Assets
         public Vector3[] Normals;
 
         public float[] Area;
-        public Vector3[] GlobalCenter;
-        public Vector3[] FaceNormal;
+        public Vector3[] WorldTriangleCenter;
+        public Vector3[] WorldTriangleNormal;
         public Vector3[] MeshTriangleVelocities;
         public Vector3[] LastTrianglePosition;
         public float[] Angle;
@@ -33,8 +33,8 @@ namespace Assets
             Normals = DragMesh.normals;
 
             Area = new float[Triangles.Length / 3];
-            GlobalCenter = new Vector3[Triangles.Length / 3];
-            FaceNormal = new Vector3[Triangles.Length / 3];
+            WorldTriangleCenter = new Vector3[Triangles.Length / 3];
+            WorldTriangleNormal = new Vector3[Triangles.Length / 3];
             MeshTriangleVelocities = new Vector3[Triangles.Length / 3];
             LastTrianglePosition = new Vector3[Triangles.Length / 3];
             Angle = new float[Triangles.Length / 3];
@@ -61,50 +61,44 @@ namespace Assets
 
         void FixedUpdate()
         {
-            Triangles = DragMesh.triangles;
-            Vertices = DragMesh.vertices;
-            Normals = DragMesh.normals;
-
             //Force at specific triangle selected in loop
             for (int triangle = 0; triangle < Triangles.Length; triangle += 3)
             {
                 int i = triangle / 3;
 
-                GlobalCenter[i] = (transform.TransformPoint(Vertices[Triangles[triangle]]) +
-                                   transform.TransformPoint(Vertices[Triangles[triangle + 1]]) +
-                                   transform.TransformPoint(Vertices[Triangles[triangle + 2]])) /
-                                  3;
+                WorldTriangleCenter[i] = (transform.TransformPoint(Vertices[Triangles[triangle]]) +
+                                          transform.TransformPoint(Vertices[Triangles[triangle + 1]]) +
+                                          transform.TransformPoint(Vertices[Triangles[triangle + 2]])) /
+                                         3;
 
-                FaceNormal[i] = (transform.TransformDirection(Normals[Triangles[triangle]]) +
-                                 transform.TransformDirection(Normals[Triangles[triangle + 1]]) +
-                                 transform.TransformDirection(Normals[Triangles[triangle + 2]])) /
-                                3;
+                WorldTriangleNormal[i] = (transform.TransformDirection(Normals[Triangles[triangle]]) +
+                                          transform.TransformDirection(Normals[Triangles[triangle + 1]]) +
+                                          transform.TransformDirection(Normals[Triangles[triangle + 2]])) /
+                                         3;
 
-                MeshTriangleVelocities[i] = (GlobalCenter[i] - LastTrianglePosition[i]) / Time.fixedDeltaTime;
+                MeshTriangleVelocities[i] = (WorldTriangleCenter[i] - LastTrianglePosition[i]) / Time.fixedDeltaTime;
 
-                LastTrianglePosition[i] = GlobalCenter[i];
+                LastTrianglePosition[i] = WorldTriangleCenter[i];
 
-                Angle[i] = Vector3.Angle(MeshTriangleVelocities[i] + WorldWind, FaceNormal[i]);
+                Angle[i] = Vector3.Angle(MeshTriangleVelocities[i] + WorldWind, WorldTriangleNormal[i]);
 
                 ForceMagnitude[i] = (float)(0.5 * 1.2 * -Mathf.Pow(MeshTriangleVelocities[i].magnitude + WorldWind.magnitude, 2) * Mathf.Clamp(Mathf.Cos(Angle[i] * Mathf.Deg2Rad), 0, 1) * Area[i]);
 
-                Force[i] = FaceNormal[i] * ForceMagnitude[i];
+                Force[i] = WorldTriangleNormal[i] * ForceMagnitude[i];
 
-                DragRigidbody.AddForceAtPosition(Force[i], GlobalCenter[i]);
+                DragRigidbody.AddForceAtPosition(Force[i], WorldTriangleCenter[i]);
 
-                //Debug.DrawLine(GlobalCenter[td3], GlobalCenter[td3] + FaceNormal[td3]);
-
-                //if (ForceMagnitude[i] != 0)
-                //{
-                //    Debug.DrawLine(GlobalCenter[i], GlobalCenter[i] + MeshTriangleVelocities[i].normalized, Color.green);
-                //}
-                //else
-                //{
-                //    Debug.DrawLine(GlobalCenter[i], GlobalCenter[i] + MeshTriangleVelocities[i].normalized, Color.red);
-                //}
-
-                //Debug.DrawLine(GlobalCenter[i], GlobalCenter[i] + Force[i], Color.green);
+                DebugDrag(i);
             }
+        }
+
+        protected void DebugDrag(int i)
+        {
+            //Debug.DrawLine(WorldTriangleCenter[td3], WorldTriangleCenter[td3] + WorldTriangleNormal[td3]);
+
+            Debug.DrawLine(WorldTriangleCenter[i], WorldTriangleCenter[i] + MeshTriangleVelocities[i].normalized, ForceMagnitude[i] != 0 ? Color.green : Color.red);
+
+            Debug.DrawLine(WorldTriangleCenter[i], WorldTriangleCenter[i] + Force[i], Color.green);
         }
     }
 }
